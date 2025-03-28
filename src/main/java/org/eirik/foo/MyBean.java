dc412b1dbaf812025854f60089d1592b97124ffc
Eirik Brænd <eirik.brend@bidbax.no>
2025-03-28 21:16:27 +0100

    clean up datasource

--- src/main/java/org/eirik/foo/MyBean.java
+++ src/main/java/org/eirik/foo/MyBean.java
@@ -2,25 +2,11 @@ package org.eirik.foo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class MyBean {
 
-    private final DataSource dataSource;
-
-
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void dbTest() {
-//        JdbcClient create =  JdbcClient.create(dataSource);
-//        Integer number = create.sql("select 42").query(Integer.class).single();
-//        log.info("the number {}", number);
-//    }
 }
